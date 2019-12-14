170f9677075f21f7db31285941fc9fcf14ec645b
gwuah <griffithawuah15@gmail.com>
2019-12-14 18:47:15 +0000

    faster version

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,24 +1,14 @@
 def bubble_sort(array) :
     has_swapped = True
     array_length = len(array)
-    x = 0
 
-    for outer_index, outer_element in enumerate(array) :
-        for inner_index, inner_element in enumerate(array) :
-            x += 1
+    for i in range(len(array)) :
+        for j in range(i) :
+            smaller = array[i]
+            if (array[i] < array[j]) :
+                array[i] = array[j]
+                array[j] = smaller
             
-            next_index = inner_index + 1 
-            greater_than_array_length = next_index > array_length - 1
-
-            if (not greater_than_array_length and inner_element > array[next_index]) :
-                current_element = inner_element
-                next_element = array[next_index]
-                # print("swapping {},{} ---({})".format(current_element, next_element, inner_element))
-
-                array[inner_index] = next_element
-                array[next_index] = current_element
-            
-    print(x)
     return array
 
     
