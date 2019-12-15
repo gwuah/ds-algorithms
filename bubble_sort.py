079ebd47cf87f0f591fc6bd798f51aa1858dc806
gwuah <griffithawuah15@gmail.com>
2019-12-15 19:03:11 +0000

    [final]

--- bubble_sort.py
+++ bubble_sort.py
@@ -3,16 +3,11 @@ def bubble_sort(array) :
     array_length = len(array)
 
     for i in range(len(array)) :
-        print("i == {}".format(i))
         for j in range(i) :
-            print("j == {}".format(j))
-
             smaller = array[i]
             if (array[j] > array[i]) :
                 array[i] = array[j]
                 array[j] = smaller
-        print("--------------------")
-            
     return array
 
     
